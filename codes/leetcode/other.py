2d7a152865bb1066fef66040ce434a301db9ba19
duyan <duyanb@enn.cn>
2024-06-21 18:15:19 +0800

    llm

--- codes/leetcode/other.py
+++ codes/leetcode/other.py
@@ -38,32 +38,22 @@ class StringAlgorithm:
         else:
             return "Neither"
 
+
+    def length_of_longest_substring(self):
+        """无重复字符串的最长子串
+        (LCR 016) 给定一个字符串 s，找出其中不含有重复字符的最长连续子字符串的长度.
+        思路：
+        时O(); 空O()
+        """
+
+
 # 最大无重复字串长度
 
 
+import math
 import torch
 import torch.nn as nn
 from typing import Optional
-
-
-class RotaryEmbedding(nn.Module):
-    def __init__(self, dim, max_tokens=2048, base=10000, device=None):
-        super().__init__()
-        # 计算位置编码的频率并加入缓存 $1 / 10000^(2k / d)$
-        inv_freq = 1.0 / (base ** (torch.arange(
-            0, dim, 2).float().to(device) / dim))
-        t = torch.arange(
-            self.max_tokens,
-            device=inv_freq.device,
-            dtype=inv_freq.dtype)
-        freqs = torch.outer(t, inv_freq)  # 向量外积
-        emb = torch.cat((freqs, freqs), dim=-1)  # 矩阵横向拼接
-
-        emb_cos = emb.cos()[None, None, :, :]  # 扩充维度
-        emb_sin = emb.sin()[None, None, :, :]
-
-
-        return emb_cos[:, :, :seq_len, ...].to(dtype=x.dtype), emb_sin[:, :, :seq_len, ...].to(dtype=x.dtype),
 
 
 class Attention(nn.Module):
@@ -85,11 +75,9 @@ class Attention(nn.Module):
         self.o_proj = nn.Linear(self.num_heads * self.head_dim,
                                 self.hidden_size, bias=False)
 
-        # self.rotary_emb = RotaryEmbedding(
-        #     self.head_dim, max_position_embeddings=self.max_position_embeddings)
-
     def forward(self, hidden_states: torch.Tensor,
-                attention_mask: Optional[torch.Tensor] = None) -> torch.Tensor:
+                attention_mask: Optional[torch.Tensor] = None,
+                position_ids: Optional[torch.LongTensor] = None) -> torch.Tensor:
         bsz, q_len, _ = hidden_states.size()
 
         query_states = self.q_proj(hidden_states).view(
@@ -103,30 +91,23 @@ class Attention(nn.Module):
 
         # [1, 1, seq_len, head_dim]
         cos, sin = self.rotary_emb(self.head_dim, self.max_tokens)
-        cos, sin = cos.to(dtype=value_states.dtype), sin.to(dtype=value_states.dtype)
+        cos = cos.to(dtype=value_states.dtype)
+        sin = sin.to(dtype=value_states.dtype)
 
         query_states, key_states = self.apply_rotary_pos_emb(
             query_states, key_states, cos, sin, position_ids)
         # [bsz, num_heads, seq_len, head_dim]
 
-        # 如果有过去存储的键值对，则进行拼接
-        if past_key_value is not None:
-            # reuse k, v, self_attention
-            key_states = torch.cat([past_key_value[0], key_states], dim=2)
-            value_states = torch.cat([past_key_value[1], value_states],
-                                     dim=2)
-
-        attn_weights = torch.matmul(query_states, key_states.transpose(2, 3)
-                                    ) / math.sqrt(self.head_dim)
-        assert attn_weights.size() == (
-        bsz, self.num_heads, q_len, kv_seq_len)
+        attn_weights = torch.matmul(
+            query_states, key_states.transpose(2, 3)) / math.sqrt(self.head_dim)
+        assert attn_weights.size() == (bsz, self.num_heads, q_len, kv_seq_len)
 
         # 如果有注意力掩码，则进行掩码处理
         if attention_mask is not None:
             assert attention_mask.size() == (bsz, 1, q_len, kv_seq_len)
             attn_weights = attn_weights + attention_mask
-            attn_weights = torch.max(attn_weights, torch.tensor(
-                torch.finfo(attn_weights.dtype).min))
+            attn_weights = torch.max(
+                attn_weights, torch.tensor(torch.finfo(attn_weights.dtype).min))
 
         # 计算注意力输出 (注意 fp32)
         attn_weights = nn.functional.softmax(
@@ -134,12 +115,10 @@ class Attention(nn.Module):
             query_states.dtype)
         attn_output = torch.matmul(attn_weights, value_states)
 
-        assert attn_output.size() == (
-        bsz, self.num_heads, q_len, self.head_dim)
+        assert attn_output.size() == (bsz, self.num_heads, q_len, self.head_dim)
 
         attn_output = attn_output.transpose(1, 2)
         attn_output = attn_output.reshape(bsz, q_len, self.hidden_size)
-
         attn_output = self.o_proj(attn_output)
 
         return attn_output
@@ -158,12 +137,6 @@ class Attention(nn.Module):
         emb_sin = emb.sin()[None, None, :, :]
 
         return emb_cos[:, :, :seq_len, ...], emb_sin[:, :, :seq_len, ...]
-
-    def _shape(self, tensor: torch.Tensor, seq_len: int, bsz: int):
-        """将张量重塑为 [batch_size, num_attention_heads, seq_len, head_dim]。
-        """
-        return tensor.view(bsz, seq_len, self.num_heads,
-                           self.head_dim).transpose(1, 2).contiguous()
 
     @staticmethod
     def rotate_half(x):
