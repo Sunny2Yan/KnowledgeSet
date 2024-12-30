cf9abd0769069a680e1e387de3a7012614b54fec
duyan <duyan@tju.edu.cn>
2024-12-30 20:22:27 +0800

    add flash code

--- codes/llm/tot.py
+++ codes/llm/tot.py
@@ -236,6 +236,6 @@ import argparse
 
 args = argparse.Namespace(backend='gpt-4', temperature=0.7, task='game24', naive_run=False, prompt_sample=None, method_generate='propose', method_evaluate='value', method_select='greedy', n_generate_sample=1, n_evaluate_sample=3, n_select_sample=5)
 
-task = Game24Task()
+task = TextTask()
 ys, infos = solve(args, task, 900)
 print(ys[0])
